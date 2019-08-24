28960524664c81ca7d57e1ba463d4e9696c7ff53
wangkang <>
2019-08-24 10:42:08 +0800

    publish all

---
+++ src/github.md
@@ -0,0 +1,10 @@
+…or create a new repository on the command line
+echo "# vue-blog-wang" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/wangkang960822/vue-blog-wang.git
+git push -u origin master
+…or push an existing repository from the command line
+git remote add origin https://github.com/wangkang960822/vue-blog-wang.git
+git push -u origin master
--- src/router.js
+++ src/router.js
@@ -1,25 +1,10 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import ShowBlogs from "./components/ShowBlog"
+import AddBlog from "./components/AddBlog"
+import SingleBlog from "./components/SingleBlog"
 
-Vue.use(Router)
-
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }
-  ]
-})
+export default [
+    { path: "/", component: ShowBlogs },
+    { path: "/add", component: AddBlog },
+    { path: "/blog/:id", component: SingleBlog },
+]
+//:id   id代表参数  与SingleBlog.vue中的created中的id相同
